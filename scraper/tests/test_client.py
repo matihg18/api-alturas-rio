d0d9ef0b84306677a00c1c3101c6c9b6c7c32de8
matihg18 <mati2004hg@gmail.com>
2026-02-14 22:45:07 -0300

    style: fix linting issues

--- scraper/tests/test_client.py
+++ scraper/tests/test_client.py
@@ -3,7 +3,7 @@ from scraper.client import PrefecturaClient
 
 
 def test_fetch_data_success(mocker, html_ejemplo_prefectura, monkeypatch):
-    monkeypatch.setenv("SOURCE_URL","http://test-url.com")
+    monkeypatch.setenv("SOURCE_URL", "http://test-url.com")
 
     mock_response = mocker.Mock()
     mock_response.status_code = 200
@@ -19,7 +19,7 @@ def test_fetch_data_success(mocker, html_ejemplo_prefectura, monkeypatch):
 
 
 def test_fetch_data_http_error(mocker, monkeypatch):
-    monkeypatch.setenv("SOURCE_URL","http://test-url.com")
+    monkeypatch.setenv("SOURCE_URL", "http://test-url.com")
     mock_response = mocker.Mock()
     mock_response.status_code = 500
     mock_response.raise_for_status.side_effect = requests.exceptions.HTTPError()
@@ -33,7 +33,7 @@ def test_fetch_data_http_error(mocker, monkeypatch):
 
 
 def test_fetch_data_timeout(mocker, monkeypatch):
-    monkeypatch.setenv("SOURCE_URL","http://test-url.com")
+    monkeypatch.setenv("SOURCE_URL", "http://test-url.com")
     mocker.patch("requests.get", side_effect=requests.exceptions.Timeout)
 
     client = PrefecturaClient()
