3f20c515cffaefa371e44219488ff830f153ac81
matihg18 <mati2004hg@gmail.com>
2026-02-14 22:43:42 -0300

    test: fix client test

--- scraper/tests/test_client.py
+++ scraper/tests/test_client.py
@@ -2,7 +2,9 @@ import requests
 from scraper.client import PrefecturaClient
 
 
-def test_fetch_data_success(mocker, html_ejemplo_prefectura):
+def test_fetch_data_success(mocker, html_ejemplo_prefectura, monkeypatch):
+    monkeypatch.setenv("SOURCE_URL","http://test-url.com")
+
     mock_response = mocker.Mock()
     mock_response.status_code = 200
     mock_response.text = html_ejemplo_prefectura
@@ -16,7 +18,8 @@ def test_fetch_data_success(mocker, html_ejemplo_prefectura):
     assert requests.get.called
 
 
-def test_fetch_data_http_error(mocker):
+def test_fetch_data_http_error(mocker, monkeypatch):
+    monkeypatch.setenv("SOURCE_URL","http://test-url.com")
     mock_response = mocker.Mock()
     mock_response.status_code = 500
     mock_response.raise_for_status.side_effect = requests.exceptions.HTTPError()
@@ -29,7 +32,8 @@ def test_fetch_data_http_error(mocker):
     assert result == ""
 
 
-def test_fetch_data_timeout(mocker):
+def test_fetch_data_timeout(mocker, monkeypatch):
+    monkeypatch.setenv("SOURCE_URL","http://test-url.com")
     mocker.patch("requests.get", side_effect=requests.exceptions.Timeout)
 
     client = PrefecturaClient()
