c4a40d229a6388a0a1a3ac54e52f7413e578c5a7
matihg18 <mati2004hg@gmail.com>
2026-02-23 11:39:53 -0300

    test: fix repository tests

--- api/tests/test_repository.py
+++ api/tests/test_repository.py
@@ -5,7 +5,7 @@ from datetime import date
 
 def test_get_ports_list(db_session, seed_data):
     repository = ApiRepository(db_session)
-    params = PagingParams(skip = 0, limit = 2)
+    params = PagingParams(skip = 0, limit = 2, sorting = None)
     result = repository.get_port_list(params)
 
     assert result.total_count == 3
@@ -27,21 +27,21 @@ def test_get_port_by_id(db_session, seed_data):
 
 def test_get_port_with_active_alert(db_session, seed_data):
     repository = ApiRepository(db_session)
-    params = PagingParams(skip = 0, limit = 10)
+    params = PagingParams(skip = 0, limit = 10, sorting = None)
     result = repository.get_ports_with_active_alert(params)
     assert result.total_count == 2
     assert result.items[0].id == 1
 
 def test_get_port_with_evacuation_alert(db_session, seed_data):
     repository = ApiRepository(db_session)
-    params = PagingParams(skip = 0, limit = 10)
+    params = PagingParams(skip = 0, limit = 10, sorting = None)
     result = repository.get_ports_with_evacuation_alert(params)
     assert result.total_count == 1
     assert result.items[0].id == 2
 
 def test_get_measurement_by_port_id(db_session, seed_data):
     repository = ApiRepository(db_session)
-    params = PagingParams(skip = 0, limit = 10)
+    params = PagingParams(skip = 0, limit = 10, sorting = None)
     date_filters = DateFilters(from_date = None, to_date = None)
     result = repository.get_measurements_by_port_id(
         port_id = 1,
